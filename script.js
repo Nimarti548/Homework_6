e8b5fa0bdb9a1733f2297d58251e9bbe5c973bd2
Nicolas Martinez <Nimarti548@gmail.com>
2020-11-07 15:06:57 -0800

    filled in the current weather function to update data, and added styles to the uv index

--- script.js
+++ script.js
@@ -1,86 +1,96 @@
 const searchBtn = document.getElementById("citySearchBtn");
 const cityInput = document.getElementById("cityInput");
 
+$("form").on("submit", function(e){
+  e.preventDefault()
+    //create a list item to append
+    let input= $("#cityInput").val();
+    let item = $("<li>")
+    item.text(input)
+    $("#citySrchHistory").prepend(item)
+    //function to return old info
+    // $("#citySrchHistory").on("click", getCurrentWeather())
 
-
-function formSubmitHandler(e) {
-    e.preventDefault();
-    let city = cityInput.value.trim();
+    getCurrentWeather(input);
     
-    if (city) {
-        getCurrentWeather(city);
-  
-      cityInput.value = '';
-    } else {
-      
-    }
-  };
+})
 
 function getCurrentWeather(city){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=b96b52626f78508367acbb00dd591cd2`)
       .then(response => response.json())
-         // save city into local storage
-
-        //create html elements with response.data
-
-        // call other functions
-        
       .then(data => {
-
-          getForecast(city);
-          // console.log(data)
-          getUVIndex(data.coord.lat, data.coord.lon)
-          
+        //fill html elements with response.data
+        let iconImg = data.weather[0].icon;
+        let currentDay = moment(data.dt_txt).format("L");
+          $("#info").text(data.name + " " + currentDay)
+          $("#icon1").attr("src", "https://openweathermap.org/img/w/" + iconImg + ".png")
+          $("#temperature").text("Temp: " +data.main.temp + "F")
+          $("#feelsLike").text("Feels Like: " + data.main.feels_like + "F")
+          $("#humidity").text("Humidity: " + data.main.humidity + "%")
+          $("#windSpeed").text("Wind Speed: " + data.wind.speed + "Mph")
+          // call other functions
+        getForecast(city);
+        getUVIndex(data.coord.lat, data.coord.lon)
         })
-        //console.log("fetching")
+        
 }
-
+// function that retrieves the forcast from the api and a for loop to loop through the data
 function getForecast(city){
   fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=b96b52626f78508367acbb00dd591cd2`)
     .then(response => response.json())
     .then(data => {
-      console.log(data)
+      $("#dataRow").empty();
+      for (let i = 0; i < data.list.length; i+= 8){
+        dataCard(data.list[i]);
+      }
     })
   }
-  
+  //function that retrieves the uv index from api and conditional to differenciate levels of uv exposure
   function getUVIndex(lat, lon) {
     fetch(`https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=b96b52626f78508367acbb00dd591cd2`)
     .then(response => response.json())
     .then(data => {
       $("#uvIndex").text(data.value);
       if (data.value <= 2){
-        $("#uvIndex").attr("class", "safe");
+        $("#uvIndex").attr("style", "background-color: Green; color: Black; width: 35px;");
       } else if (data.value <= 5) {
-        $("#uvIndex").attr("class", "moderate");
+        $("#uvIndex").attr("style", "background-color: Yellow; color: Black; width: 35px;");
       } else if (data.value <= 7) {
-        $("#uvIndex").attr("class", "high");
+        $("#uvIndex").attr("style", "background-color: Orange; color: Black; width: 35px;");
       } else if (data.value <= 10) {
-        $("#uvIndex").attr("class", "veryHigh");
+        $("#uvIndex").attr("style", "background-color: Red; color: White; width: 35px;");
       } else if (data.value > 10)
-      $("#uvIndex").attr("class", "danger");
+      $("#uvIndex").attr("style", "background-color: Purple; color: White; width: 35px;");
     })
-    $("#card-row").empty();
-    for (let i = 0; i < data.list.length; i+= 8){
-      cardInformer(data.list[i]);
-    }
   }
 
-  let cardInformer = function (data){
+  let dataCard = function (data){
+    //created information tags for the card
     let iconImg = data.weather[0].icon;
     let card = $("<div>");
-    let body = $("<div>");
     let head = $("<h4>");
+    let body = $("<div>");
     let icon = $("<img>");
     let temp = $("<p>");
     let humidity = $("<p>");
-
-    card.attr("style", "width: 160px; margin-right 10px")
-    icon.attr("src", "https://openweathermap.org/img/w/" + iconcode + ".png")
-
-
-
-  }
+    // added classes to those tags with some styles
+    card.attr("style", "width: 160px; margin-right 10px; border: solid; padding: 10px;")
+    icon.attr("src", "https://openweathermap.org/img/w/" + iconImg + ".png")
+    body.addClass("daily-forcast");
+    head.addClass("days");
+    // added text to those tags
+    head.text(moment(data.dt_txt).format("L"));
+    temp.text("Temp: " + data.main.temp + " F");
+    humidity.text("Humidity: " + data.main.humidity + "%");
+    // appended all the tags so they can build the card 
+  body.append(head);
+  body.append(icon);
+  body.append(temp);
+  body.append(humidity);
+  card.append(body);
+  $("#dataRow").append(card);
+}
   
-searchBtn.addEventListener("click", formSubmitHandler);
+
 
 // $("#temp").text(data.list[0].main.temp)
